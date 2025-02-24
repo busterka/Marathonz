1abc34f29b8f294d57e5047a814bf8ae6b893d21
prr03 <prr03@DESKTOP-OO6HC9L>
2025-02-24 13:52:47 +0700

    Добавьте файлы проекта.

---
+++ Marathon/Class/Core.cs
@@ -0,0 +1,14 @@
+﻿using Marathon.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marathon.Class
+{
+    class Core
+    {
+        public static MarathonSkillsEntities DB = new MarathonSkillsEntities();
+    }
+}
